97d0561231fd1d37055582a2ab99c3ca31189858
Rafal Bazan <rafalbe777@gmail.com>
2025-01-23 22:35:51 +0100

    Data streaming from the terminal to LV

---
+++ dataLeft.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Open a persistent TCP connection
+exec 3<>/dev/tcp/127.0.0.1/12346
+
+# Source ROS setup files
+source /opt/ros/humble/setup.bash
+source /home/rafal/TrailblazerML/install/local_setup.bash
+
+# Publish ROS messages and redirect output to TCP socket
+ros2 topic echo /diff_drive_controller_right/odom | awk '/linear:/ {getline; print $2}' | \
+while read -r line; do 
+    echo "$line" >&3
+    sleep 0.1 # Opóźnienie 0.5 sekundy
+done
+
+# Keep the script running until manually stopped
+wait
+
+# Close the connection when the script exits
+exec 3>&-
