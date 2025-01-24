e68ef62398104ff2422ebf535f785ad7b5c62433
Rafal Bazan <rafalbe777@gmail.com>
2025-01-24 13:13:12 +0100

    Real rovel controlling addon

--- rosRight.sh
+++ rosRight.sh
@@ -8,7 +8,9 @@ source /home/rafal/TrailblazerML/install/local_setup.bash
 
 pkill -f "diff_drive"
 
-ros2 topic pub --rate 100 /diff_drive_controller_left/cmd_vel_unstamped geometry_msgs/msg/Twist "{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
+( ros2 topic pub --rate 100 /diff_drive_controller_left/cmd_vel_unstamped geometry_msgs/msg/Twist "{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}" & \
+ros2 topic pub --once /diff_drive_controller_left_labview/cmd_vel_unstamped geometry_msgs/Twist "{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}" & \
+ros2 topic pub --once /diff_drive_controller_right_labview/cmd_vel_unstamped geometry_msgs/Twist "{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}" )
 
 # Keep the script running until manually stopped
 wait
---
+++ rosStop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Open a persistent TCP connection
+exec 3<>/dev/tcp/127.0.0.1/12345
+
+source /opt/ros/humble/setup.bash
+source /home/rafal/TrailblazerML/install/local_setup.bash
+
+pkill -f "diff_drive"
+
+
+( ros2 topic pub --once /diff_drive_controller_left_labview/cmd_vel_unstamped geometry_msgs/Twist "{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}" & \
+ros2 topic pub --once /diff_drive_controller_right_labview/cmd_vel_unstamped geometry_msgs/Twist "{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}")
+
+# Keep the script running until manually stopped
+wait
+
+# Close the connection when the script exits
+exec 3>&-
